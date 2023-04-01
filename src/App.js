255667ac7b9c36087d7a7e2610f0e9218452b380
karloslazaroo <101194055+karloslazaroo@users.noreply.github.com>
2023-04-02 01:38:14 +0800

    df

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
-    <div>hello</div>
+    <div>
+      
+      <Navbar/>
+    
+    </div>
   );
 }
 
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './navbar.css'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <ul>
+            <li><a>Home</a></li>
+            <li><a>About</a></li>
+            <li><a>Contact</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
